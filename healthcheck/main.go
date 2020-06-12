7844c7db2db34559730bb5c9cf97fe06ffab9471
lhz <pepitsizous@zoho.com>
2020-06-12 12:46:33 +0300

    add env var  for healthcheck program

--- healthcheck/main.go
+++ healthcheck/main.go
@@ -1,14 +1,23 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"os"
 )
 
 func main() {
-	r, err := http.Get("http://localhost")
+
+	URL := os.Getenv("URL")
+	if URL == "" {
+		URL = "http://localhost"
+	}
+
+	_, err := http.Get(URL)
+	fmt.Println("getting", URL)
 	if err != nil {
 		os.Exit(1)
 	}
+	fmt.Println("exit status 0")
 	os.Exit(0)
 }
