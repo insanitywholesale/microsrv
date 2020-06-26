e3ac9856a80b234dfa676f0c3465af1e98563db0
lhz <pepitsizous@zoho.com>
2020-06-26 12:50:56 +0300

    comment healthcheck

--- healthcheck/main.go
+++ healthcheck/main.go
@@ -8,20 +8,28 @@ import (
 
 func main() {
 
+	// use URL from env var but
+	// if empty default to localhost
 	URL := os.Getenv("URL")
 	if URL == "" {
 		URL = "http://localhost"
 	}
 
+	// perform GET request
 	fmt.Println("getting:", URL)
 	r, err := http.Get(URL)
 	if err != nil {
 		os.Exit(1)
 	}
+
+	// check status code
 	fmt.Println("status code:", r.StatusCode)
 	if r.StatusCode != 200 {
 		os.Exit(1)
 	}
+
+	// print exit message formatted
+	// same as os.Exit(1) does
 	fmt.Println("exit status 0")
 	os.Exit(0)
 }
